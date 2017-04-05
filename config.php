5215f8fba05267477ac61452d1f1e8c646c690ea
Stephanie Williams <sadamson@email.unc.edu>
2017-04-05 08:52:42 -0400

    clearing config for release

--- config.php
+++ config.php
@@ -1,14 +1,14 @@
 <?php
 
 // DPLA Submission Checker Configuration
-        $provider = "North Carolina Digital Heritage Center"; // DPLA Service Hub Name
-        $oaibaseurl = "http://repox.lib.unc.edu:8080/repox/OAIHandler"; // DPLA Service Hub Base URL
+        $provider = ""; // DPLA Service Hub Name
+        $oaibaseurl = ""; // DPLA Service Hub Base URL
         $metadataprefix = "MODS";  // If this changes, please edit stylesheets in /xsl accordingly
-        $harvestday = "13"; // Day of the month DPLA is scheduled to harvest
-        $helpcontact = "Lisa Gregory"; // Name of Help Contact Person
-        $helpemail = "digitalnc@unc.edu"; // Email address for Help requests
-        $helpphone = "(919) 962-4345"; // Phone number for Help requests
-        
-        // DO NOT EDIT
+        $harvestday = ""; // Day of the month DPLA is scheduled to harvest, e.g., "20"
+        $helpcontact = ""; // Name of Help Contact Person
+        $helpemail = ""; // Email address for Help requests
+        $helpphone = ""; // Phone number for Help requests
+
+        // DO NOT EDIT UNLESS CHANGING DATAPROVIDER REQUIREMENT
         $dataprovider = isset($_GET['dataprovider']) ? $_GET['dataprovider'] : '';
 ?>
